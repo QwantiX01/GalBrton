d89314a1549bae890374f150949db2b4adad7ef6
Yurii Bizhik <qwantiix01@gmail.com>
2024-02-10 22:43:06 +0200

    Done contact us

--- GalBeton/src/components/Forms/ContactForm.tsx
+++ GalBeton/src/components/Forms/ContactForm.tsx
@@ -2,43 +2,68 @@ interface ContactFormProps {}
 
 const ContactForm = ({}: ContactFormProps) => {
   return (
-    <form className="mt-8 w-min">
-      <div className="flex flex-col gap-4">
-        <div className="flex gap-6">
-          <div className="flex flex-col items-start gap-4">
-            <label htmlFor="firstName" className="text-xl">
-              Ім'я
-            </label>
-            <input
-              id="firstName"
-              className="focus: outline-none bg-black p-3 border-solid border-gray-600 border-2 rounded-xl"
-              placeholder="Ваше ім'я"
-              type="text"
-            />
+    <div className="flex items-end">
+      <form className="mt-8 w-min border-solid border-gray-600 border-[1px] rounded-xl p-5">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-6">
+            <div className="flex flex-col items-start gap-4">
+              <label htmlFor="firstName" className="text-xl">
+                Ім'я*
+              </label>
+              <input
+                id="firstName"
+                className="focus: outline-none bg-black px-3 py-4 border-solid border-gray-600 border-[1px] rounded-xl"
+                placeholder="Ваше ім'я"
+                type="text"
+              />
+            </div>
+            <div className="flex flex-col items-start gap-4">
+              <label htmlFor="lastName" className="text-xl">
+                Прізвище*
+              </label>
+              <input
+                id="lastName"
+                className="focus: outline-none bg-black px-3 py-4 border-solid border-gray-600 border-[1px] rounded-xl"
+                placeholder="Ваше Прізвище"
+                type="text"
+              />
+            </div>
           </div>
-          <div className="flex flex-col items-start gap-4">
-            <label htmlFor="lastName" className="text-xl">
-              Прізвище
-            </label>
-            <input
-              id="lastName"
-              className="focus: outline-none bg-black p-3 border-solid border-gray-600 border-2 rounded-xl"
-              placeholder="Ваше Прізвище"
-              type="text"
-            />
-          </div>
+          <label htmlFor="number" className="text-xl">
+            Номер телефону
+          </label>
+          <input
+            id="number"
+            className="focus: outline-none bg-black px-3 py-4 border-solid border-gray-600 border-[1px] rounded-xl"
+            placeholder="Ваш номер телефону"
+            type="text"
+          />
+          <label htmlFor="gmail" className="text-xl">
+            Eлектронна пошта
+          </label>
+          <input
+            id="gmail"
+            className="focus: outline-none bg-black px-3 py-4 border-solid border-gray-600 border-[1px] rounded-xl"
+            placeholder="Ваша електронна пошта"
+            type="text"
+          />
+          <label htmlFor="message" className="text-xl">
+            Додаткова інформація
+          </label>
+          <textarea
+            id="message"
+            className="focus: outline-none bg-black px-3 py-4 border-solid border-gray-600 border-[1px] rounded-xl h-52 break-words"
+            placeholder="Введіть ваше повідомлення..."
+          />
         </div>
-        <label htmlFor="lastName" className="text-xl">
-          електронна пошта
-        </label>
-        <input
-          id="lastName"
-          className="focus: outline-none bg-black p-3 border-solid border-gray-600 border-2 rounded-xl"
-          placeholder="Ваша електронна пошта"
-          type="text"
-        />
-      </div>
-    </form>
+      </form>
+      <button
+        type="submit"
+        className="bg-white text-black px-4 ml-auto py-2 rounded-xl font-mono font-bold"
+      >
+        Надіслати
+      </button>
+    </div>
   );
 };
 
--- GalBeton/src/pages/Contact.tsx
+++ GalBeton/src/pages/Contact.tsx
@@ -1,11 +1,12 @@
 import ContactForm from "../components/Forms/ContactForm";
+import ContactInfo from "../components/Forms/ContactInfo";
 
 interface ContactProps {}
 
 const Contact = ({}: ContactProps) => {
   return (
     <>
-      <main className="mx-44 mt-9">
+      <main className="mx-56 mt-9 p-6">
         <h1 className="text-center text-5xl font-bold mb-4">
           Зв'яжіться з Гал<span className="">Бетон</span>
         </h1>
@@ -13,7 +14,26 @@ const Contact = ({}: ContactProps) => {
           Наша команда завжди готова відповісти на будь-яке ваше запитання. Ми з
           нетерпінням чекаємо на ваш дзвінок
         </p>
+        <h1 className="mt-8 text-2xl font-bold">Заповніть форму:</h1>
         <ContactForm />
+        <h1 className="my-8 text-2xl font-bold">
+          Або зв'яжіться з нами особисто
+        </h1>
+        <ContactInfo
+          icon="call"
+          title={"123-456-7891"}
+          description={"8:00 - 17:00"}
+        />
+        <ContactInfo
+          icon="send"
+          title={"Email"}
+          description={"qwantiix01@gmail.com"}
+        />
+        <ContactInfo
+          icon="location_on"
+          title={"Адреса"}
+          description={"вул. Луганська 123"}
+        />
       </main>
     </>
   );
